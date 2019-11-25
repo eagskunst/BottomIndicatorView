1534aca3045b0ef604f3156a33942add6024f73d
eagskunst(WSL) <emmanuelnypd@gmail.com>
2019-11-25 19:46:12 -0400

    Created MultiListenerBottomNavigationView and IndicatorView. Code for the Multilistener

---
+++ bottomindicatorview2/src/main/java/com/eagskunst/libraries/bottomindicatorview/BottomNavigationIndicatorView.kt
@@ -0,0 +1,16 @@
+package com.eagskunst.libraries.bottomindicatorview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Created by eagskunst in 25/11/2019.
+ */
+
+class BottomNavigationIndicatorView  @JvmOverloads constructor(
+    context: Context, attributeSet: AttributeSet? = null, defStyleAttr: Int = 0):
+        View(context, attributeSet, defStyleAttr) {
+
+
+}
---
+++ bottomindicatorview2/src/main/java/com/eagskunst/libraries/bottomindicatorview/MultiListenerBottomNavigationView.kt
@@ -0,0 +1,41 @@
+package com.eagskunst.libraries.bottomindicatorview
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MenuItem
+import androidx.core.view.forEachIndexed
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+/**
+ * Created by eagskunst in 25/11/2019.
+ */
+
+class MultiListenerBottomNavigationView @JvmOverloads constructor
+    (context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0): BottomNavigationView(context, attrs, defStyleAttr),
+    BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private val listeners = mutableListOf<OnNavigationItemSelectedListener>()
+
+    init {
+        super.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun setOnNavigationItemSelectedListener(listener: OnNavigationItemSelectedListener?) {
+        if(listener != null) listeners.add(listener)
+    }
+
+    fun addOnNavigationItemSelectedListener(listener: OnNavigationItemSelectedListener){
+        listeners.add(listener)
+    }
+
+    inline fun addOnNavigationItemSelectedListener(crossinline listener: (Int) -> Unit){
+        addOnNavigationItemSelectedListener(OnNavigationItemSelectedListener {
+            menu.forEachIndexed { index, item -> if (item == it) listener(index) }
+            false
+        })
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem) = listeners.map { it.onNavigationItemSelected(item) }
+        .fold(false){ acc, it -> acc || it }
+
+}
